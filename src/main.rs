7aaf28902f7d9b9edfa344622760fd23c5f4c05a
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2022-12-14 14:11:43 -0400

    CLI Arguments. Reading File.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
+#![allow(unused)]
+use clap::{arg, Parser};
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    let pattern = std::env::args().nth(1).expect("No Pattern Given");
-    let path = std::env::args(2).expect("No Path Given");
-    println!("Hello, world!");
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("Could Not Read File");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+    // let pattern = std::env::args().nth(1).expect("No Pattern Given");
+    // let path = std::env::args().nth(2).expect("No Path Given");
+    // let args = Cli {
+    //     path: std::path::PathBuff::from(path),
+    //     pattern,
+    // };
+    // println!("Hello, world!");
 }
